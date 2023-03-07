962d99ec939b2f6192ccad66d9b1c6496b1ab9ef
Nicos-08 <thierrylemoine136@gmail.com>
2023-03-07 09:13:01 +0100

    Composant FilterList fonctionnel

--- src/components/Filter/FilterList/FilterList.jsx
+++ src/components/Filter/FilterList/FilterList.jsx
@@ -1,21 +1,18 @@
-import "../../../styles/components/Tag/TagList/TagList.css";
+import "../../../styles/components/Filter/FilterList/FilterList.css";
 import FilterItem from "../FilterItem/FilterItem"
 
-const FilterList = ({ tags }) => {
+const FilterList = ({ filterItems }) => {
     return (
-        <div className="tagList">
-            <h5>Tags :</h5>
-            <div className="tagList-container">
-                {tags.map((tag, index) => (
-                    <FilterItem key={`${tag}-${index}`} name={tag} />
-                ))}
-            </div>
+        <div className="filterList">
+            {filterItems.map((item, index) => (
+                <FilterItem key={`${item.itemName}-${index}`} itemName={item.itemName} dashed={item.dashed} />
+            ))}
         </div>
     )
 }
 
 FilterList.defaultProps = {
-    tags: ["Tag1", "Tag2", "Tag3"]
+    filterItems: [{itemName: "Item1", dashed: false}, {itemName: "Item2", dashed: true}, {itemName: "Item3", dashed: false}]
 }
 
 export default FilterList
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import FilterItem from './components/Filter/FilterItem/FilterItem';
+import FilterList from './components/Filter/FilterList/FilterList';
 import "./styles/base/default.css"
 import "./styles/base/fonts.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FilterItem />
+    <FilterList filterItems={[{itemName: "loul", dashed:false}]} />
   </React.StrictMode>
 );
