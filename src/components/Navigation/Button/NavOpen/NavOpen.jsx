76336ecd95f7e245d74710c62633fcd78477dbe0
Nicos-08 <thierrylemoine136@gmail.com>
2023-03-07 18:38:11 +0100

    Composant NavOpen fonctionnel

---
+++ src/components/Navigation/Button/NavOpen/NavOpen.jsx
@@ -0,0 +1,13 @@
+import "../../../../styles/components/Navigation/Button/NavOpen/NavOpen.css";
+
+const NavOpen = () => {
+    return (
+        <div className="navOpen">
+            <div className="navOpen-line"></div>
+            <div className="navOpen-line"></div>
+            <div className="navOpen-line"></div>
+        </div>
+    )
+}
+
+export default NavOpen
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import NavClose from './components/Navigation/Button/NavClose/NavClose';
+import NavOpen from './components/Navigation/Button/NavOpen/NavOpen';
 import "./styles/base/default.css"
 import "./styles/base/fonts.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavClose />
+    <NavOpen />
   </React.StrictMode>
 );
