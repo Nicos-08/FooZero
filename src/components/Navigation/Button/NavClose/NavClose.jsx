a28ccc4470e96191a2bc49b0f10c61b2c2d8e5e0
Nicos-08 <thierrylemoine136@gmail.com>
2023-03-07 18:33:14 +0100

    Composant NavClose fonctionnel

---
+++ src/components/Navigation/Button/NavClose/NavClose.jsx
@@ -0,0 +1,12 @@
+import "../../../../styles/components/Navigation/Button/NavClose/NavClose.css";
+
+const NavClose = () => {
+    return (
+        <div className="navClose">
+            <div className="navClose-line1"></div>
+            <div className="navClose-line2"></div>
+        </div>
+    )
+}
+
+export default NavClose
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PaginationDefault from './components/Pagination/PaginationDefault/PaginationDefault';
+import NavClose from './components/Navigation/Button/NavClose/NavClose';
 import "./styles/base/default.css"
 import "./styles/base/fonts.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PaginationDefault pages={[4, 5, 6, 7]} />
+    <NavClose />
   </React.StrictMode>
 );
