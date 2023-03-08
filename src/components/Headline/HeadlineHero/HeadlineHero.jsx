f3bc3a92eeed5036f450b89da7baf34032555ad9
Nicos-08 <thierrylemoine136@gmail.com>
2023-03-08 19:03:50 +0100

    Mise des props dans HeadlineHero

--- src/components/Headline/HeadlineHero/HeadlineHero.jsx
+++ src/components/Headline/HeadlineHero/HeadlineHero.jsx
@@ -1,15 +1,17 @@
 import "../../../styles/components/Headline/HeadlineHero/HeadlineHero.css"
 
-const HeadlineHero = ({ maxWidth }) => {
+const HeadlineHero = ({ title, subtitle, maxWidth }) => {
     return (
         <div className="heroHeading" style={maxWidth={maxWidth}}>
-            <span className="heroHeading">Healthy Eating is important part of lifestyle</span>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu</p>
+            <span className="heroHeading">{title}</span>
+            <p>{subtitle}</p>
         </div>
     )
 }
 
 HeadlineHero.defaultProps = {
+    title: "Healthy Eating is important part of lifestyle",
+    subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque congue arcu",
     maxWidth: "1057px"
 }
 
