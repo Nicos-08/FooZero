9e2e7be497719a7fae1a1dc899323bbaf32a4667
Nicos-08 <thierrylemoine136@gmail.com>
2023-03-19 11:55:44 +0100

    Création d'un fichier à duppliquer

---
+++ Base/Base.jsx
@@ -0,0 +1,15 @@
+import "../../../styles/components/BaseFolder/Base/Base.css";
+
+const Base = () => {
+    return (
+        <div className="base">
+
+        </div>
+    )
+}
+
+Base.defaultProps = {
+
+}
+
+export default Base;
